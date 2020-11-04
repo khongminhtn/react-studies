4b960c8e9325c0bf2b5323a281535b8ced4c1af6
khongminhtn <tuyenminhkhong@live.co.uk>
2020-11-04 17:41:39 +0000

    added a life cycle method componentWillReceiveProps() to collect the following props verify whether it is a modal.

--- book-store/jsx/app.jsx
+++ book-store/jsx/app.jsx
@@ -64,7 +64,38 @@ const Copy = () => {
 
 
 class App extends React.Component {
+    // takes following properties as its argument
+    // good place to determine whether this view is a modal
+    componentWillReceiveProps(nextProps) {
+        this.isModal = (nextProps.location.state 
+            && nextProps.location.state.modal)
+        if (this.isModal 
+            && nextProps.location.key 
+            !== this.props.location.key) {
+            this.previousChildren = this.props.children
+        } 
+    }
 
+    render() {
+        console.log('Modal: ', this.isModal)
+        return (
+            <div className="well">
+                <Heading/>
+                <div>
+                    {(this.isModal) ? 
+                    this.previousChildren : this.props.children}
+
+                    {(this.isModal) ?
+                        <Modal 
+                        isOpen={true} 
+                        returnTo={this.props.location.state.returnTo}>  
+                            {this.props.children}
+                        </Modal> : ""
+                    }
+                </div>
+            </div>
+        )
+    }
 }
 
 
