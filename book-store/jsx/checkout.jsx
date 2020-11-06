7b23bfab1fc969ae5a6fa977b14e3f3cdaa2ea6a
khongminhtn <tuyenminhkhong@live.co.uk>
2020-11-06 13:15:50 +0000

    added checkout.jsx component

---
+++ book-store/jsx/checkout.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class Checkout extends React.Component {
+    return() {
+        let count = 0
+        return (
+            <div>
+                <h1>Invoice</h1>
+                <table className="table table-bordered">
+                    <tbody>
+                        {Object.keys(this.props.route.cartItems).map((item, index, list)=>{
+                            count += this.props.route.cartItems[item]
+                            return (
+                                <tr key={item}>
+                                    <td>{this.props.route.products[item].title}</td>
+                                    <td>{this.props.route.cartItems[item]}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+                <p>Total: {count}</p>
+            </div>
+        )
+    }
+}
+
+export {Checkout}
