3fff8b76856d321e4439ba651ff532af63cbe46a
khongminhtn <tuyenminhkhong@live.co.uk>
2020-11-06 12:42:43 +0000

    added cart.jsx component

---
+++ book-store/jsx/cart.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class Cart extends React.Component {
+    render() {
+        return (
+            <div>
+                {(Object.keys(this.props.route.cartItems).length == 0) 
+                ? <p>Your cart is empty</p>
+                : ""}
+
+                <ul>
+                    {Object.keys(this.props.route.cartItems).map((item, index, list) => {
+                        return <li key={item}>
+                            {this.props.route.products[item].title}
+                            - {this.props.route.cartItems[item]}
+                        </li>
+                    })}
+                </ul>
+
+                <Link 
+                    to="/checkout"
+                    className="btn btn-primary">
+                    checkout
+                </Link>
+
+                <Link
+                    to="/"
+                    className="btn btn-info">
+                    Home
+                </Link>
+            </div>
+        )
+    }
+};
